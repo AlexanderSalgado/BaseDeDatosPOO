2c1f840674c39991a9ff2c5aa403ebc115cd5fab
Rodolfo Salgado <00191617@uca.edu.sv>
2018-06-16 04:21:54 -0600

    Base de datos con POO.

---
+++ src/inventario/Inventario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventario;
+
+import vista.Consulta;
+
+/**
+ *
+ * @author rodol
+ */
+
+public class Inventario {
+
+    public static void main(String[] args) {
+        new Consulta().setVisible(true);
+    }
+
+}
